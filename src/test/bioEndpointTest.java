a7d43f0b7570c8f07ecd207b9c9f6a8300272717
sealfon <sealfon@99bf2634-c001-11de-afc4-699a8c260df9>
2009-11-08 19:14:11 +0000

    bioEndpoint test updated & now works

--- src/test/bioEndpointTest.java
+++ src/test/bioEndpointTest.java
@@ -6,19 +6,28 @@ import com.hp.hpl.jena.query.*;
 public class bioEndpointTest {
 	  public static void main(String arguments[]){
 	    	try {
-	    		System.out.println("hello");
-	    		String service = "http://www4.wiwiss.fu-berlin.de/dailymed/sparql";
-	    		String query = "SELECT ?name ?indication WHERE {?x dailymed:indication ?indication FILTER regex(?indication, 'coronary artery disease', 'i') ?x rdfs:label ?name}";
-	    		QueryExecution e  = QueryExecutionFactory.sparqlService(service, query);
-	    		System.out.println("hello2");
-//	    		System.out.println("hello");
-//	    		String service =  "http://www4.wiwiss.fu-berlin.de/diseasome/sparql";
-//	    		String query = "SELECT ?disease WHERE {?x rdfs:label ?disease FILTER regex(?disease, '^coronary artery disease$', 'i') }";
-//	    		QueryExecution e  = QueryExecutionFactory.sparqlService(service, query);
-//	    		System.out.println("hello2");
-
-	    		ResultSet results = e.execSelect();
 	    		
+	    		String service =  "http://www4.wiwiss.fu-berlin.de/diseasome/sparql";
+	    		String query = "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> SELECT ?disease WHERE {?x rdfs:label ?disease FILTER regex(?disease, '^coronary artery disease$', 'i') }";
+	    		QueryExecution e  = QueryExecutionFactory.sparqlService(service, query);
+	    		
+	    		ResultSet results = e.execSelect();
+
+	    		while (results.hasNext()){
+	    			QuerySolution s = results.next();
+	    			System.out.println(s);
+	    		}
+	    		
+	    		service = "http://www4.wiwiss.fu-berlin.de/dailymed/sparql";
+	    		query = "PREFIX dailymed: <http://www4.wiwiss.fu-berlin.de/dailymed/resource/dailymed/> PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> SELECT ?name ?indication WHERE {?x dailymed:indication ?indication FILTER regex(?indication, 'coronary artery disease', 'i') ?x rdfs:label ?name}";
+	    		e  = QueryExecutionFactory.sparqlService(service, query);
+	    		
+	    		 results = e.execSelect();
+
+	    		 while (results.hasNext()){
+		    			QuerySolution s = results.next();
+		    			System.out.println(s);
+	    		 }
 
 //	    		List<String> l = results.getResultVars();
 //	    	
@@ -26,15 +35,12 @@ public class bioEndpointTest {
 //	    			System.out.println(i+" : "+l.get(i));
 //	    		}
 
-	    		while (results.hasNext()){
-	    			QuerySolution s = results.next();
-	    			System.out.println(s);
-
+	    		
 	 //   			System.out.println(s.get("name"));
-	    		}
+	    		
 	    		
 	    	} 
-	    	catch (Exception e) {
+	    	catch (Exception e) {System.err.println(e);
 	        }
 	    }
 }
