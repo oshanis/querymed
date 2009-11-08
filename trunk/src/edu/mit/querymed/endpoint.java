9dce72d1ce9d4e222e410d0d89d0f2bfc21e0395
oshanis <oshanis@99bf2634-c001-11de-afc4-699a8c260df9>
2009-11-08 15:32:22 +0000

    Added the initial source code for querying a SPARQL endpoint

---
+++ trunk/src/edu/mit/querymed/endpoint.java
@@ -0,0 +1,17 @@
+package edu.mit.querymed;
+
+public class endpoint {
+
+	String uri;
+	boolean supportsREST = true;
+	
+	public endpoint(String uri){
+		this.uri = uri;
+	}
+
+	public endpoint(String uri, boolean supportsREST){
+		this.uri = uri;
+		this.supportsREST = supportsREST;
+	}
+
+}
---
+++ trunk/src/edu/mit/querymed/endpointAggregator.java
@@ -0,0 +1,8 @@
+package edu.mit.querymed;
+
+public class endpointAggregator {
+	
+
+}
+
+
