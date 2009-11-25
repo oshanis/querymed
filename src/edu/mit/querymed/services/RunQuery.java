047cf275c74567cfe7ed24162dc96f699290fbb2
oshanis <oshanis@99bf2634-c001-11de-afc4-699a8c260df9>
2009-11-25 00:39:27 +0000

    Added json jquery

--- src/edu/mit/querymed/services/RunQuery.java
+++ src/edu/mit/querymed/services/RunQuery.java
@@ -54,64 +54,24 @@ public class RunQuery extends HttpServlet{
 	 		}
 	
 	        int dailymedCount=0;
+//	        while (dailymedResults.hasNext()){
+//	 			QuerySolution dailymedSolution = dailymedResults.next();
+//	 			jsonDailymed.put("name"+(dailymedCount++),dailymedSolution.get("name"));
+//	 			jsonDailymed.put("indication"+(dailymedCount++),dailymedSolution.get("indication"));
+//	        }
 	        while (dailymedResults.hasNext()){
 	 			QuerySolution dailymedSolution = dailymedResults.next();
-	 			jsonDailymed.put("name"+(dailymedCount++),dailymedSolution.get("name"));
-	 			jsonDailymed.put("indication"+(dailymedCount++),dailymedSolution.get("indication"));
+	 			jsonDailymed.put("name",dailymedSolution.get("name"));
+//	 			jsonDailymed.put("indication",dailymedSolution.get("indication"));
 	        }
 	         
 	         //Finaly add the 2 JSONObjects to the JSONObjects that we are returning to the client
-         	jsonResp.put(DISEASOME_ENDPOINT, jsonDiseasome);
-            jsonResp.put(DAILYMED_ENDPOINT, jsonDailymed);
+         	//jsonResp.put("diseasome", jsonDiseasome);
+            jsonResp.put("dailymed", jsonDailymed);
             out.write(jsonResp.toString());
     	} 
     	catch (Exception e) {
     		System.err.println(e);
         }	
-		
-//        try{
-//
-//        	String keyword = req.getParameter("keyword");
-//		
-//        	String diseasomeQuery = Util.prefixes + " SELECT ?disease WHERE {?x rdfs:label ?disease FILTER regex(?disease, '" + keyword  + "', 'i') }";
-//		     
-//		   //  String diseasomeQuery = Util.prefixes + " SELECT ?disease WHERE {?x rdfs:label ?disease FILTER regex(?disease, '" + keyword  + "', 'i') }";
-//		     String dailymedQuery = Util.prefixes + " SELECT ?name ?indication WHERE {?x dailymed:indication ?indication FILTER regex(?indication, '" + keyword +"', 'i') ?x rdfs:label ?name}";
-//		
-//		     ResultSet diseasomeResults = QueryExecutionFactory.sparqlService("http://www4.wiwiss.fu-berlin.de/diseasome/sparql",diseasomeQuery).execSelect();
-//		     ResultSet dailymedResults = QueryExecutionFactory.sparqlService("http://www4.wiwiss.fu-berlin.de/dailymed/sparql",dailymedQuery).execSelect();
-//		
-//           // The following generates a page showing all the request parameters
-//           PrintWriter out = resp.getWriter();
-//           
-//           //Sending the response as a json type
-//           resp.setContentType("application/json");
-//
-//           //Send the results as a JSONobject
-//		     JSONObject jsonResp = new JSONObject();
-//		     
-//		     //JSON objects the endpoints send their values in
-//		     JSONObject jsonDiseasome = new JSONObject();
-//		     JSONObject jsonDailymed = new JSONObject();
-//	    	 int dieseasomeCount=0;
-//	         while (diseasomeResults.hasNext()){
-//	 			QuerySolution diseasomeSolution = diseasomeResults.next();
-//	 			jsonDiseasome.put("disease"+(dieseasomeCount++),diseasomeSolution.get("disease"));
-//	 		}
-//	
-//	         int dailymedCount=0;
-//	         while (dailymedResults.hasNext()){
-//	 			QuerySolution dailymedSolution = dailymedResults.next();
-//	 			jsonDailymed.put("name"+(dailymedCount++),dailymedSolution.get("name"));
-//	 		//	jsonDailymed.put("indication"+(dailymedCount++),dailymedSolution.get("indication"));
-//	         }
-//	         //Finaly add the 2 JSONObjects to the JSONObjects that we are returning to the client
-//	        
-//	         jsonResp.put("http://www4.wiwiss.fu-berlin.de/diseasome/sparql", jsonDiseasome);
-//	         //jsonResp.put("http://www4.wiwiss.fu-berlin.de/dailymed/sparql", jsonDailymed);
-//	         out.write(diseasomeQuery+"\n\n\n"+jsonResp.toString());
-//        }
-//        catch(Exception e){}
-
 	}
 }
